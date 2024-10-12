b20f733c33ea5e5fc25c3a2939f76dde7e4afce3
Manikandan <43571823+infinire@users.noreply.github.com>
2024-10-12 13:05:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          DemoApp-React
+          DemoApp-mani
         </a>
       </header>
     </div>
