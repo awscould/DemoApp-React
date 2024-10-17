cea11fb8fd233caa88b534ef3861f65b124d01b1
Manikandan <43571823+awscould@users.noreply.github.com>
2024-10-17 17:02:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ReactPipeline-App
+          Pipeline setup EC2
         </a>
       </header>
     </div>
