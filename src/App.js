bc0aeb0ea8889e00adbfa495c84c8bd0c866a5a0
Manikandan <43571823+awscould@users.noreply.github.com>
2024-10-17 16:34:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ReactPipeline
+          ReactPipeline-App
         </a>
       </header>
     </div>
